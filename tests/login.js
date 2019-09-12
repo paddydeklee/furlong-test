21af70f0af612a3fcc5149347070244ca0c74b0d
Paddy de Klee <paddydeklee@gmail.com>
2019-09-12 19:06:24 +0100

    added comments

--- tests/login.js
+++ tests/login.js
@@ -1,4 +1,4 @@
-const { openBrowser, evaluate, checkBox, press, goto, waitFor, screenshot, screencast, toRightOf, button, inputField, textBox, focus, write, into, click, doubleClick, $, closeBrowser } = require('taiko');
+const { openBrowser, evaluate, checkBox, press, goto, screenshot, screencast, toRightOf, button, inputField, textBox, focus, write, into, click, doubleClick, $, closeBrowser, waitFor } = require('taiko');
 var _selectors = require('./selectors')
 
 step("Go to edc study <study>", async(study) => {
@@ -10,6 +10,9 @@ step("Go to edc study <study>", async(study) => {
     await click($(".PrimaryButton"));
     await $(study).exists();
     await click(study);
+    console.log("study clicked");
+    waitFor(2000);
+    console.log("waited for 2 secs");
 });
 
 // step("Click button <selector>", async(selector) => {
@@ -22,18 +25,26 @@ step("Click button with selector <selector>", async function(selector) {
 });
 
 step("Add new record with ID <recordID>", async function(recordID) {
+    console.log("adding new record" + recordID);
     await press(['Alt', 'n']);
     await write(recordID);
     await press("Tab");
     await press("Enter");
+    waitFor(2000);
 });
 
 step("Enter record <recordID>", async function(recordID) {
+    console.log("begin enter record");
     await $(recordID).exists();
     await doubleClick(recordID);
+    waitFor(2000);
+    console.log("waited 2 secs then entered record!");
+
+
 });
 
 step("Fill registration form", async function(){
+    console.log("starting to fill registration form");
     await focus(textBox(toRightOf("Date of Registration")));
     await write("02-09-2011");
     await focus(textBox(toRightOf("Consent")));
@@ -45,10 +56,9 @@ step("Fill registration form", async function(){
     await focus(textBox(toRightOf("Person taking consent")));
     await write("PdK");
     await focus(checkBox("Retrospective").check());
-    // await focus(checkBox("Gender").check());
-    // await focus(checkBox("Does the patient meet all inclusion criteria?").check());
+    await focus(checkBox("Gender").check());
+    await focus(checkBox("Does the patient meet all inclusion criteria?").check());
 })
-
 
 
 step("Click Next", async function(){
