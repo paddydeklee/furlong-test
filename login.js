30b1d1dfe2b87e79610dc4f7e0ce0fccb7cfbe0b
paddydeklee <paddydeklee@gmail.com>
2019-11-14 16:16:18 +0000

    Update login.js

--- login.js
+++ login.js
@@ -4,9 +4,9 @@ const { openBrowser, goto, inputField, focus, write, click, $, closeBrowser } = 
         await openBrowser();
         await goto("https://uk.castoredc.com/");
         await focus(inputField({id:"field-username"}));
-        await write("paddydeklee@gmail.com");
+        await write("X");
         await focus(inputField({id:"field-password"}));
-        await write("Paddy1234!");
+        await write("X");
         await click("Log in");
         await click($(".PrimaryButton"));
     } catch (error) {
